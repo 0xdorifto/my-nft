1f89f24c72b31a67ea1cb9274a63929263a43151
0xdorifto <gastaosousafaria@gmail.com>
2023-10-12 15:16:06 +0100

    Setup hardhat configuration

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,25 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-verify";
+import * as dotenv from "dotenv";
+import { HardhatUserConfig } from "hardhat/config";
+
+dotenv.config({ path: "/.env" });
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY!;
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 
 const config: HardhatUserConfig = {
   solidity: "0.8.19",
+  networks: {
+    sepolia: {
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY,
+  },
 };
 
 export default config;
